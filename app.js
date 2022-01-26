ae9916e6aa29d915656266a78e57200ecfdc3637
visneviySecret <visneviy.secret@yandex.ru>
2022-01-26 07:49:15 +0300

    paggination + a bit optimization

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const header = document.querySelector('.table__header');
-
 (async () => {
     const JSONdata = await(await fetch('data.json')).json();
     tableMaker(JSONdata);
 })();
 
-const sortName = document.querySelector('.table__header').firstElementChild
-const sortPhone = document.querySelector('.table__header').firstElementChild.nextElementSibling
-const sortAbout = document.querySelector('.table__header').firstElementChild.nextElementSibling.nextElementSibling
-const sortEyeColor = document.querySelector('.table__header').firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling
+const header = document.querySelector('.table__header');
+const sortName = header.firstElementChild
+const sortPhone = header.firstElementChild.nextElementSibling
+const sortAbout = header.firstElementChild.nextElementSibling.nextElementSibling
+const sortEyeColor = header.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling
 const table = document.querySelector('#table')
 const tbody = document.getElementById('table').getElementsByTagName("TBODY")[0]
 const edit = document.createElement('div')
@@ -20,22 +19,14 @@ const aboutEdit = document.createElement('input')
 const eyeColorEdit = document.createElement('input')
 const submitButton = document.createElement('button')
 
-
-nameEdit.classList.add('form__edit')
-phoneEdit.classList.add('form__edit')
-aboutEdit.classList.add('form__edit')
-eyeColorEdit.classList.add('form__edit')
 submitButton.classList.add('form__submit')
-
-
 submitButton.textContent = 'Submit changing'
 submitButton.type = 'submit'
 
-
-edit.appendChild(nameEdit)
-edit.appendChild(phoneEdit)
-edit.appendChild(aboutEdit)
-edit.appendChild(eyeColorEdit)
+for (let i of [nameEdit, phoneEdit, aboutEdit, eyeColorEdit]){
+  i.classList.add('form__edit')
+  edit.appendChild(i)
+}
 edit.appendChild(submitButton)
 
 function tableMaker(data) {
@@ -53,77 +44,101 @@ function tableMaker(data) {
     
         let newRow = document.createElement('tr')
         newRow.classList.add('table__row')
-        newRow.appendChild(name)
-        newRow.appendChild(phone)
-        newRow.appendChild(about)
-        newRow.appendChild(eyeColor)
-        
+
+        for (let i of [name, phone, about, eyeColor]){
+          newRow.appendChild(i)
+        }
+        tbody.appendChild(newRow)
+    }
+    table.removeChild(tbody)
+
+    const pagDiv = document.createElement('tbody')
+    //pagDiv.classList.add('tbody')
+    const pagContainer = document.querySelector('.container')
+    table.firstElementChild.after(pagDiv)
+    const quantityOfSection = 10
+    const numberOfButton = 5
+
+    function paintPaginationButton(count) {
+      for (i=1, r=""; i <= count; i++){
+        r += `<button class="pb">${i}<button/>`
+      }
+      return r
+    }
+    function paintResult (arr){
+      for (i = 0, r = []; i < arr.length; i++){
+        let newRow = arr[i]
+        r.push(newRow)
+      }
+      return r
+    }
+    const block = document.createElement('div')
+    block.classList.add('block')
+    block.innerHTML = paintPaginationButton(numberOfButton)
+    Array.from(block.childNodes).map((child, index) => { if (index % 2 !== 0 ) {block.removeChild(child)}}) //скрипт убирающий ошибку с возникновение дополнительных элементов <button> в пагинации
+
+    let result = []
+    result.push(paintResult(Array.from(tbody.childNodes)).slice(1, quantityOfSection+1))
+
+    function addRow ([result]){
+      for (let i of result){
+        let newRow = i.cloneNode(true)
         newRow.addEventListener('click', function adder(event){
           if(event.target.parentNode.tagName === 'DIV') return // предотвращение входа в цикл, нужно, чтобы присваивать div в строку таблицы
           if (event.target.parentNode.lastElementChild.classList.value === 'table__edit') {
               newRow.removeChild(newRow.lastChild)
-              
               return
             };
-            
-           
-            let name = event.target.parentNode.firstElementChild.textContent
-            let phone = event.target.parentNode.firstElementChild.nextElementSibling.textContent
-            let about = event.target.parentNode.firstElementChild.nextElementSibling.nextElementSibling.textContent
-            let eyeColor = event.target.parentNode.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.textContent
+            let rowArray = Array.from(event.target.parentNode.childNodes)
+            let rowEdit = edit.childNodes
 
-
-            edit.firstElementChild.value = name
-            edit.firstElementChild.nextElementSibling.value = phone
-            edit.firstElementChild.nextElementSibling.nextElementSibling.value = about
-            edit.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.value = eyeColor
-            
-
+            for (let i in rowArray){  //записываем значения строки в поле для редактирования
+              rowEdit[i].value = rowArray[i].textContent
+            }
             newRow.appendChild(edit)
             
+            submitButton.addEventListener('click', (event)=>{ //перезапись полей в таблице
 
-            submitButton.addEventListener('click', (event)=>{
-              let newName = event.target.parentNode.firstElementChild.value
-              let newPhone = event.target.parentNode.firstElementChild.nextElementSibling.value
-              let newAbout = event.target.parentNode.firstElementChild.nextElementSibling.nextElementSibling.value
-              let newEyeColor = event.target.parentNode.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.value
-              
-              event.target.parentNode.parentNode.firstElementChild.innerHTML = newName
-              event.target.parentNode.parentNode.firstElementChild.nextElementSibling.innerHTML = newPhone
-              event.target.parentNode.parentNode.firstElementChild.nextElementSibling.nextElementSibling.innerHTML = newAbout
-              event.target.parentNode.parentNode.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = newEyeColor
-              console.log(newName, newPhone, newAbout, newEyeColor)
+              let submitValues = Array.from(event.target.parentNode.childNodes)
+              let newValues = Array.from(event.target.parentNode.parentNode.childNodes)
+
+              for (let i = 0; i <= 3; i++){
+                newValues[i].innerHTML = submitValues[i].value
+              }
             })
-            /*innerHTML: ""    innerText: ""*/
         })
-
-        tbody.appendChild(newRow)
-   
+        pagDiv.appendChild(newRow)
+      }
     }
-    
-}
-sortName.addEventListener('click', () =>{
-    let sortedRows = Array.from(table.rows)
-  .slice(1)
-  .sort((rowA, rowB) => rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? 1 : -1);
-  table.tBodies[0].append(...sortedRows);
-})
-sortPhone.addEventListener('click', () =>{
-    let sortedRows = Array.from(table.rows)
-  .slice(1)
-  .sort((rowA, rowB) => rowA.cells[1].innerHTML > rowB.cells[1].innerHTML ? 1 : -1);
-  table.tBodies[0].append(...sortedRows);
-})
-sortAbout.addEventListener('click', () =>{
-    let sortedRows = Array.from(table.rows)
-  .slice(1)
-  .sort((rowA, rowB) => rowA.cells[2].innerHTML > rowB.cells[2].innerHTML ? 1 : -1);
-  table.tBodies[0].append(...sortedRows);
-})
-sortEyeColor.addEventListener('click', () =>{
-    let sortedRows = Array.from(table.rows)
-  .slice(1)
-  .sort((rowA, rowB) => rowA.cells[3].innerHTML > rowB.cells[3].innerHTML ? 1 : -1);
-  table.tBodies[0].append(...sortedRows);
-})
+    addRow(result)
 
+    pagContainer.appendChild(block)
+
+    document.addEventListener('click', function(event){
+      if([...event.target.classList].includes("pb")){
+         
+         let y = event.target.textContent
+         let start = quantityOfSection*(y-1)+1
+         let end = quantityOfSection*y+1
+         
+         result = []
+         result.push(paintResult(Array.from(tbody.childNodes)).slice(start, end))
+         while (pagDiv.firstChild){
+           pagDiv.removeChild(pagDiv.firstChild)
+         }
+         addRow(result)
+      }
+   })
+}
+//toDO создать асинзронную функцию принимающую tbody и переписывающую значения строчек в ней при сортировке
+/*
+//сортировка столбцов, подходящая для структуры table
+[sortName, sortPhone, sortAbout, sortEyeColor].map((item, index) => {
+  item.addEventListener('click',()=>{
+    let sortedRows = Array.from(table.rows)
+    .slice(1)
+    .sort((rowA, rowB) => rowA.cells[index].innerHTML > rowB.cells[index].innerHTML ? 1 : -1);
+    table.tBodies[0].append(...sortedRows);
+  })
+})
+*/
